5861018c91a5a5b820f9892e80055f9c0d2d18ba
Mike Moore <mike.moore@openlogic.com>
2017-02-09 13:33:29 -0700

    Removed archive file for build

--- build/docker_it.sh
+++ build/docker_it.sh
@@ -10,6 +10,5 @@ tar -c -z -v --exclude=application/node_modules -C ../ -f $DOCKER_DIR/applicatio
 echo "Build the image with Dockerfile in docker dir"
 docker-compose build
 
-
 rm -rf $DOCKER_DIR/application.tg
 
