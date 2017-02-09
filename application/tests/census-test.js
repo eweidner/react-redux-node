1348d2bdede16d863d21a7f8816e18fa2959a9cb
Mike Moore <mike.moore@openlogic.com>
2017-02-09 13:27:53 -0700

    Initial commit - express init and work on dockerization

---
+++ application/tests/census-test.js
@@ -0,0 +1,6 @@
+/**
+ * Created by mikemoore on 2/9/17.
+ */
+const assert = require('assert');
+
+//assert.assert(false, "Intentional faile");
---
+++ build/docker_it.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BUILD_DIR=$(pwd)
+DOCKER_DIR=$BUILD_DIR/docker
+
+echo "Tar the application into Docker dir for copying to image"
+rm -rf $DOCKER_DIR/application.tg
+tar -c -z -v --exclude=application/node_modules -C ../ -f $DOCKER_DIR/application.tgz application/
+
+echo "Build the image with Dockerfile in docker dir"
+docker-compose build
+
+
+rm -rf $DOCKER_DIR/application.tg
+
